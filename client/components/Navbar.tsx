ca96ae67eb44268109fa92b7e765a0961e228b13
mmonj <mmonj@users.noreply.github.com>
2024-11-02 23:39:25 -0400

    update navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Context } from "@reactivated";
+import { Context, reverse } from "@reactivated";
 import { Navbar as BSNavbar, Container, Nav } from "react-bootstrap";
 
 import { NavLink } from "./Navlink";
@@ -9,13 +9,13 @@ export function Navbar() {
   const djangoContext = React.useContext(Context);
 
   return (
-    <BSNavbar expand="lg" className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <BSNavbar expand="lg" className="navbar navbar-expand navbar-light navbar-bg">
       <Container fluid>
         <BSNavbar.Brand href={"/fix-me"}>Home</BSNavbar.Brand>
-        <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
-        <BSNavbar.Collapse id="basic-navbar-nav">
+        <BSNavbar.Toggle aria-controls="navbar-nav" />
+        <BSNavbar.Collapse>
           <Nav className="me-auto mb-2 mb-lg-0">
-            <NavLink href={"/fix-me"}>Add Classes</NavLink>
+            <NavLink href={reverse("course_searcher:add_classes")}>Add Classes</NavLink>
           </Nav>
 
           <Nav className="mb-2 mb-lg-0">
