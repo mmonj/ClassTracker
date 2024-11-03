7c416c3ba5bb537eb1ecc09543270e4f2d0cf6be
mmonj <mmonj@users.noreply.github.com>
2024-11-02 21:05:16 -0400

    init types

---
+++ client/types.ts
@@ -0,0 +1,40 @@
+export type ApiResponse<T> = {
+  json: () => Promise<T>;
+} & Response;
+
+export type ApiPromise<T> = Promise<ApiResponse<T>>;
+
+interface ReactivatedSerializationWidgetsDateTimeAttrs {
+  id: string;
+  disabled?: boolean;
+  required?: boolean;
+  placeholder?: string;
+  format: string;
+}
+
+export interface DjangoFormsWidgetsDateTimeInput {
+  template_name: "django/forms/widgets/datetime.html";
+  name: string;
+  is_hidden: boolean;
+  required: boolean;
+  value: string | null;
+  attrs: ReactivatedSerializationWidgetsDateTimeAttrs;
+  type: "text";
+  tag: "django.forms.widgets.DateTimeInput";
+}
+
+export interface IHttpError {
+  detail: string;
+}
+
+export type TNotFoundErrorList = string[];
+
+export type TUseFetchResult<T> =
+  | {
+      ok: false;
+      data: null;
+    }
+  | {
+      ok: true;
+      data: T;
+    };
