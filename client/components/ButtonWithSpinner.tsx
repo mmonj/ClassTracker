4bb9f2d2a713b01fda1e635a6971e97f9d398624
mmonj <mmonj@users.noreply.github.com>
2024-11-05 13:23:31 -0500

    add ButtonWithSpinner component

---
+++ client/components/ButtonWithSpinner.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { Spinner } from "react-bootstrap";
+
+interface Props {
+  /** Whether to replace children elements with loading spinner or display beside them */
+  hideChildren?: boolean;
+  isLoadingState: boolean;
+  isIdentifierMatching?: boolean;
+  spinnerVariant?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+  onClick?: () => void;
+  size: "sm" | "md";
+  type: "button" | "submit" | "reset" | undefined;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function ButtonWithSpinner({
+  className = "",
+  spinnerVariant = "primary",
+  hideChildren = false,
+  isIdentifierMatching = true,
+  ...props
+}: Props) {
+  const trueSize = props.size === "md" ? undefined : "sm";
+  const masterIsHideChildren = hideChildren && props.isLoadingState && isIdentifierMatching;
+
+  return (
+    <button
+      className={className}
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.isLoadingState}
+    >
+      {!masterIsHideChildren && <> {props.children} </>}
+      {props.isLoadingState && isIdentifierMatching && (
+        <span>
+          <Spinner variant={spinnerVariant} animation="border" role="status" size={trueSize}>
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </span>
+      )}
+    </button>
+  );
+}
