10ebafff9b19624760451af5ec6b04ea106c4623
mmonj <mmonj@users.noreply.github.com>
2025-02-12 22:21:09 -0500

    utilize get_response_result

--- course_searcher/views.py
+++ course_searcher/views.py
@@ -1,5 +1,4 @@
 import logging
-import time
 
 from bs4 import BeautifulSoup
 from django.contrib.auth import authenticate, login, logout
@@ -9,12 +8,18 @@ from django.shortcuts import redirect
 from django.utils.encoding import iri_to_uri
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.views.decorators.http import require_GET, require_http_methods
-from requests import HTTPError, Session
+from requests import HTTPError
 from rest_framework.exceptions import APIException
 from rest_framework.exceptions import NotFound as DRFNotFound
 
+from server.util import bulk_create_and_get
+
 from . import interfaces, templates
-from .global_search.navigator import get_main_page, get_subject_selection_page
+from .global_search import init_http_retrier
+from .global_search.navigator import (
+    get_main_page,
+    get_subject_selection_page,
+)
 from .global_search.parser import create_careers_and_subjects, get_terms_available, parse_schools
 from .models import School, Term
 
@@ -81,14 +86,15 @@ def add_classes(request: HttpRequest) -> HttpResponse:
 
 
 def refresh_available_terms(request: HttpRequest) -> HttpResponse:
-    session = Session()
-    soup = BeautifulSoup(get_main_page(session), "html.parser")
+    session = init_http_retrier()
+    main_page_soup = BeautifulSoup(get_main_page(session), "html.parser")
 
     try:
-        terms_parsed = get_terms_available(soup)
+        terms_parsed = get_terms_available(main_page_soup)
     except HTTPError as ex:
         raise DRFNotFound([str(ex)]) from ex
 
+    terms_parsed = get_terms_available(main_page_soup)
     if len(terms_parsed) == 0:
         raise APIException("Parsed 0 terms")
 
@@ -100,24 +106,16 @@ def refresh_available_terms(request: HttpRequest) -> HttpResponse:
         except IntegrityError:
             continue
 
-    schools = parse_schools(soup)
-    schools.sort(key=lambda school: school.name)
+    schools = sorted(parse_schools(main_page_soup), key=lambda school: school.name)
+    schools_db = bulk_create_and_get(School, schools, unique_fieldnames=["globalsearch_key"])
 
-    schools = School.objects.bulk_create(schools, 50, ignore_conflicts=True)
-    schools_queryset = School.objects.filter(
-        globalsearch_key__in=[f.globalsearch_key for f in schools]
-    )
+    Term.objects.all().update(is_available=False)
 
-    terms_queryset = Term.objects.all()
-    terms_queryset.update(is_available=False)
-
-    terms_queryset = Term.objects.filter(
-        name__in=[f.name for f in terms_parsed], year__in=[f.year for f in terms_parsed]
-    )
-    for term in terms_queryset:
+    terms_db = Term.objects.filter(globalsearch_key__in=[f.globalsearch_key for f in terms_parsed])
+    for term in terms_db:
         term.is_available = True
-        term.save()
-        term.schools.add(*schools_queryset)
+        term.save(update_fields=["is_available"])
+        term.schools.add(*schools_db)
 
     return interfaces.BasicResponse(is_success=True, message=f"{count} new terms created").render(
         request
@@ -136,13 +134,11 @@ def refresh_semester_data(request: HttpRequest, school_id: int, term_id: int) ->
     )
 
     for school in schools:
-        session = Session()
+        session = init_http_retrier()
         subjects_page_soup = BeautifulSoup(
             get_subject_selection_page(session, school, term), "html.parser"
         )
 
         course_careers, subjects = create_careers_and_subjects(subjects_page_soup, school, term)
-
-        time.sleep(0.5)
 
     return interfaces.BasicResponse().render(request)
