e535f9475bb6628589e321e8dee89e61814d24ce
mmonj <mmonj@users.noreply.github.com>
2025-02-12 22:09:05 -0500

    add _get_payload_for_class_result_page; utilize get_response_result

--- course_searcher/global_search/navigator.py
+++ course_searcher/global_search/navigator.py
@@ -2,8 +2,8 @@ import logging
 
 from requests import Session
 
-from ..models import School, Term
-from . import get_response
+from ..models import CourseCareer, School, Subject, Term
+from . import get_response_result
 
 logger = logging.getLogger("main")
 
@@ -26,15 +26,18 @@ def get_new_search_page(session: Session, timeout: float = 10) -> str:
 
 
 def get_main_page(session: Session) -> str:
-    resp = get_response(lambda: session.get(GLOBALSEARCH_URL, timeout=10))
-    return resp.text
+    resp_result = get_response_result(lambda: session.get(GLOBALSEARCH_URL, timeout=10))
+    if not resp_result.ok:
+        raise resp_result.err
+
+    return resp_result.value.text
 
 
 def get_subject_selection_page(session: Session, school: School, term: Term) -> str:
     headers = _get_request_headers()
     data = _get_payload_for_subject_data(school, term)
 
-    resp = get_response(
+    resp_result = get_response_result(
         lambda: session.post(
             GLOBALSEARCH_URL,
             headers=headers,
@@ -43,7 +46,31 @@ def get_subject_selection_page(session: Session, school: School, term: Term) -> 
         )
     )
 
-    return resp.text
+    if not resp_result.ok:
+        raise resp_result.err
+
+    return resp_result.value.text
+
+
+def get_classlist_result_page(
+    session: Session, course_career: CourseCareer, subject: Subject
+) -> str:
+    headers = _get_request_headers()
+    data = _get_payload_for_class_result_page(course_career, subject)
+
+    resp_result = get_response_result(
+        lambda: session.post(
+            GLOBALSEARCH_URL,
+            headers=headers,
+            data=data,
+            timeout=10,
+        )
+    )
+
+    if not resp_result.ok:
+        raise resp_result.err
+
+    return resp_result.value.text
 
 
 def _get_request_headers() -> dict[str, str]:
@@ -74,4 +101,38 @@ def _get_payload_for_subject_data(school: School, term: Term) -> dict[str, str]:
         "selectedTermName": term.full_term_name,
         "term_value": term.globalsearch_key,
         "next_btn": "Next",
+    }
+
+
+def _get_payload_for_class_result_page(
+    course_career: CourseCareer, subject: Subject
+) -> dict[str, str]:
+    return {
+        "selectedSubjectName": subject.name,
+        "subject_name": subject.globalsearch_key,
+        "selectedCCareerName": course_career.name,
+        "courseCareer": course_career.globalsearch_key,
+        "selectedCAttrName": "",
+        "courseAttr": "",
+        "selectedCAttrVName": "",
+        "courseAttValue": "",
+        "selectedReqDName": "",
+        "reqDesignation": "",
+        "selectedSessionName": "",
+        "class_session": "",
+        "selectedModeInsName": "",
+        "meetingStart": "LT",
+        "selectedMeetingStartName": "less than",
+        "meetingStartText": "",
+        "AndMeetingStartText": "",
+        "meetingEnd": "LE",
+        "selectedMeetingEndName": "less than or equal to",
+        "meetingEndText": "",
+        "AndMeetingEndText": "",
+        "daysOfWeek": "I",
+        "selectedDaysOfWeekName": "include only these days",
+        "instructor": "B",
+        "selectedInstructorName": "begins with",
+        "instructorName": "",
+        "search_btn_search": "Search",
     }
