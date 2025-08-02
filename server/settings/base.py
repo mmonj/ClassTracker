593fb1b9a8278624a8d961fcd76e59579b57337f
mmonj <mmonj@users.noreply.github.com>
2025-08-02 00:59:45 -0400

    move 'reactivated' to last of INSTALLED_APPS

--- server/settings/base.py
+++ server/settings/base.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "reactivated",
     "class_tracker",
     "scheduler",
+    "reactivated",  # goes last
 ]
 
 MIDDLEWARE = [
