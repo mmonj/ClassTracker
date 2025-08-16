4520f645b44e3f1f894ee37f01febeb2e5037236
mmonj <mmonj@users.noreply.github.com>
2025-08-16 18:30:39 -0400

    fix: scheduler config `PORT` key needs to be an int

--- server/settings/scheduler_queues.py
+++ server/settings/scheduler_queues.py
@@ -1,6 +1,6 @@
 import os
-from scheduler.types import SchedulerConfiguration, Broker, QueueConfiguration
 
+from scheduler.types import Broker, QueueConfiguration, SchedulerConfiguration
 
 SCHEDULER_CONFIG = SchedulerConfiguration(
     EXECUTIONS_IN_PAGE=20,
@@ -9,20 +9,23 @@ SCHEDULER_CONFIG = SchedulerConfiguration(
     CALLBACK_TIMEOUT=60,  # Callback timeout in seconds (success/failure/stopped)
     # Default values, can be overriden per task/job
     DEFAULT_SUCCESS_TTL=10 * 60,  # Time To Live (TTL) in seconds to keep successful job results
-    DEFAULT_FAILURE_TTL=365 * 24 * 60 * 60,  # Time To Live (TTL) in seconds to keep job failure information
+    # Time To Live (TTL) in seconds to keep job failure information
+    DEFAULT_FAILURE_TTL=365 * 24 * 60 * 60,
     DEFAULT_JOB_TTL=10 * 60,  # Time To Live (TTL) in seconds to keep job information
     DEFAULT_JOB_TIMEOUT=5 * 60,  # timeout (seconds) for a job
-    # General configuration values
-    DEFAULT_WORKER_TTL=10 * 60,  # Time To Live (TTL) in seconds to keep worker information after last heartbeat
-    DEFAULT_MAINTENANCE_TASK_INTERVAL=10 * 60,  # The interval to run maintenance tasks in seconds. 10 minutes.
+    # == General configuration values ==
+    # Time To Live (TTL) in seconds to keep worker information after last heartbeat
+    DEFAULT_WORKER_TTL=10 * 60,
+    # The interval to run maintenance tasks in seconds. 10 minutes.
+    DEFAULT_MAINTENANCE_TASK_INTERVAL=10 * 60,
     DEFAULT_JOB_MONITORING_INTERVAL=30,  # The interval to monitor jobs in seconds.
     SCHEDULER_FALLBACK_PERIOD_SECS=120,  # Period (secs) to wait before requiring to reacquire locks
 )
 
 SCHEDULER_QUEUES: dict[str, QueueConfiguration] = {
-    'default': QueueConfiguration(
+    "default": QueueConfiguration(
         HOST=os.environ["REDIS_HOST"],
-        PORT=os.environ["REDIS_PORT"],
+        PORT=int(os.environ["REDIS_PORT"]),
         PASSWORD=os.environ["REDIS_PASSWORD"],
         DB=0,
     ),
