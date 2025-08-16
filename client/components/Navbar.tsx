2a5727f8832a036f1bde65356ea609118f8a45df
mmonj <mmonj@users.noreply.github.com>
2025-08-16 18:29:11 -0400

    fix navbar not being responsive

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -14,29 +14,31 @@ export function Navbar() {
   const djangoContext = React.useContext(Context);
 
   const staffRoutes = [
+    { name: "Admin", href: "/admin" },
     { name: "Manage Courselist", href: reverse("class_tracker:manage_course_list") },
     { name: "Add Classes", href: reverse("class_tracker:add_classes") },
     { name: "Class Alerts", href: reverse("class_tracker:view_class_alerts") },
   ] satisfies TStaffRoute[];
 
   return (
-    <BSNavbar expand="lg" className="navbar navbar-expand navbar-light navbar-bg">
+    <BSNavbar expand="lg">
       <Container fluid>
         <BSNavbar.Brand href={reverse("class_tracker:index")}>Home</BSNavbar.Brand>
-        <BSNavbar.Toggle aria-controls="navbar-nav" />
-        <BSNavbar.Collapse>
+        <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <BSNavbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto mb-2 mb-lg-0">
-            {staffRoutes.map((route) => (
-              <NavLink key={route.name} href={route.href}>
-                {route.name}
-              </NavLink>
-            ))}
+            {djangoContext.user.is_superuser &&
+              staffRoutes.map((route) => (
+                <NavLink key={route.name} href={route.href}>
+                  {route.name}
+                </NavLink>
+              ))}
           </Nav>
 
           <Nav className="mb-2 mb-lg-0">
             {!djangoContext.user.is_authenticated && (
               <>
-                <NavLink href={"#fix-me"}>Create Account</NavLink>
+                {/* <NavLink href={"#fix-me"}>Create Account</NavLink> */}
                 <NavLink href={reverse("class_tracker:login_view")}>Log In</NavLink>
               </>
             )}
